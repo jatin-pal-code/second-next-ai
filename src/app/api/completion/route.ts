e0e325990d1f6597db4ef322c11509df449e2efc
Jatin Pal <paljatin681@gmail.com>
2025-09-01 23:55:19 +0530

    add frontend

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -8,7 +8,8 @@ import { google } from "@ai-sdk/google";
 export async function POST() {
     const { text } = await generateText({
         model: google("models/gemini-2.0-flash-exp"),
-        prompt: "What is love?"
+        prompt: "how to became a billionaire"
         });
         return Response.json({ text });
 }
+
---
+++ src/app/ui/completion/page.ts
@@ -0,0 +1,2 @@
+cd hdwhhafhkfkhfhdh
+hdhgjjgh
